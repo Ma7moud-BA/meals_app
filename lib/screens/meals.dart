2d618343864b885c15fee49b35f81f90e059bc05
MahmoudBA <mahmoudbanyamer99@gmail.com>
2024-11-13 22:26:32 +0300

    Bottom Navigation Bar setup

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -4,9 +4,9 @@ import 'package:meals_app/screens/meal_details.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({super.key, this.title, required this.meals});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
@@ -47,9 +47,17 @@ class MealsScreen extends StatelessWidget {
         ),
       );
     }
+
+// this step is to conditionally render the Scaffold widget - since the meals screen now lives inside the tabs screen there will be two Scaffolds inside each other therefore two appBars so this is will lead to a bad UI.
+// one solution is to remove the appBar and Scaffold widget but we can;t since the meals screen is accessed in another location in the app not just from the tabs screen.
+//  so know when we set the activePage in the meals screen we don't pass the title, this will prevent rendering the appBar twice.
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
